941c5f849ca3b4a5663e84c3668923356d79c12f
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-25 19:35:10 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,11 +1,18 @@
 import pickle
 
 def predict(user_input):
-    # Load the model using pickle
-    with open("ecom.pkl", "rb") as f:
-        rf = pickle.load(f)
+    try:
+        # Load the model using pickle
+        with open("ecom.pkl", "rb") as f:
+            rf = pickle.load(f)
+        
+        print("Model loaded successfully!")
+        
+        # Make predictions
+        prediction = rf.predict([user_input])
+        
+        return prediction
     
-    # Make predictions
-    prediction = rf.predict([user_input])
-    
-    return prediction
+    except Exception as e:
+        print(f"Error loading the model: {e}")
+        return None
